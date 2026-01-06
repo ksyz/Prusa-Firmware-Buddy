34f00089170b7c695cfc92f63c2246df56d0b852
Michal Ingeli <mi@v3.sk>
2026-01-06 09:01:32 +0100

    Fixes filament still stuck in extrude after unloading.

--- include/marlin/Configuration_MINI_adv.h
+++ include/marlin/Configuration_MINI_adv.h
@@ -929,7 +929,7 @@
 // This short retract is done immediately, before parking the nozzle.
     #define FILAMENT_CHANGE_UNLOAD_FEEDRATE 80 // (mm/s) Unload filament feedrate. This can be pretty fast.
     #define FILAMENT_CHANGE_UNLOAD_ACCEL 1250 // (mm/s^2) Lower acceleration may allow a faster feedrate.
-    #define FILAMENT_CHANGE_UNLOAD_LENGTH 400 // (mm) The length of filament for a complete unload.
+    #define FILAMENT_CHANGE_UNLOAD_LENGTH 470 // (mm) The length of filament for a complete unload.
 //   For Bowden, the full length of the tube and nozzle.
 //   For direct drive, the full length of the nozzle.
 //   Set to 0 for manual unloading.
